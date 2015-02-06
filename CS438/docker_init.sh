5c534df041d3d2559dd7b8d2cfb9948abc30947a
Ray Wang <raywang945@gmail.com>
2015-02-06 01:58:32 -0600

    improve docker workflow

--- CS438/docker_init.sh
+++ CS438/docker_init.sh
@@ -1,1 +1,1 @@
-docker run -ti -v /Users/raywang/workspace/UIUC-spring-2015/CS438:/workspace raywang945/ubuntu-cs438
+docker run -ti --dns=8.8.8.8 -v /Users/raywang/workspace/UIUC-spring-2015/CS438:/workspace raywang945/ubuntu-cs438
